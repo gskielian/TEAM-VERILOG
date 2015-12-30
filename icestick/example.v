aa54ecc3af4b3ba7a45e1ced16b539c87383fc32
Max Bruckner <max@maxbruckner.de>
2015-12-30 17:31:50 +0100

    make blinking light

--- icestick/example.v
+++ icestick/example.v
@@ -11,12 +11,19 @@ module top (
 	localparam LOG2DELAY = 22;
 
 	reg [BITS+LOG2DELAY-1:0] counter = 0;
-	reg [BITS-1:0] outcnt;
+	reg out_led = 0;
 
 	always@(posedge clk) begin
-		counter <= counter + 1;
-		outcnt <= counter >> LOG2DELAY;
+		if (counter == 1000000) begin
+			out_led <= out_led ^ 1;
+			counter <= 0;
+		end
+		else
+		begin
+			counter <= counter + 1;
+		end
 	end
-	
-	assign {LED1, LED2, LED3, LED4, LED5} = outcnt ^ (outcnt >> 1);
+
+	assign {LED1, LED2, LED3, LED4} = 15;
+	assign LED5 = out_led;
 endmodule
